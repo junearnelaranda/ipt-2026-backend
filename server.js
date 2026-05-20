1af90bc200abf67cf43b6e2ff76148d7b2283076
June Arnel E. Aranda <junearnel09@gmail.com>
2026-05-21 01:17:39 +0800

    Mysql connection

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
+const db = require('./config/db');
 
 const app = express();
 
@@ -20,6 +21,25 @@ app.use(cookieParser());
 
 app.get('/', (req, res) => {
   res.json({ message: 'IPT 2026 Backend API is running' });
+});
+
+app.get('/health/db', async (req, res, next) => {
+  try {
+    const [rows] = await db.query('SELECT 1 AS ok');
+    res.json({
+      message: 'Database connection successful',
+      result: rows[0]
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).json({
+    message: 'Internal server error'
+  });
 });
 
 app.listen(port, () => {
